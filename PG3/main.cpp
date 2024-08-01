78ab30cffadd13470b5e3cac46b0559c4e46e15b
satsuki329 <k021g1414@g.neec.ac.jp>
2024-08-01 14:51:16 +0900

    一旦保存

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,6 +1,10 @@
 #include <Novice.h>
+#include "GameManager.h"
+#include "TitleScene.h"
+#include "StageScene.h"
+#include "ClearScene.h"
 
-const char kWindowTitle[] = "GC2D_06_サメシマリツキ";
+const char kWindowTitle[] = "PG3_0501";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -8,47 +12,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+	GameManager* gameManager = new GameManager();
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
+
 		Novice::BeginFrame();
 
-		// キー入力を受け取る
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
-		/// 画像読み込み
-		
+		gameManager->Run(keys, preKeys);
 
-		/// ↓更新処理ここから
-		///
-
-		///
-		/// ↑更新処理ここまで
-		///
-
-		///
-		/// ↓描画処理ここから
-
-		
-		///
-		/// ↑描画処理ここまで
-		///
-
-		// フレームの終了
-		Novice::EndFrame();
-
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0)
+		{
 			break;
 		}
+
+		Novice::EndFrame();
 	}
 
 	// ライブラリの終了
 	Novice::Finalize();
+
 	return 0;
 }
